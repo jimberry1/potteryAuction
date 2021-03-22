787cc784ff3ce82ed721ea2318d7f0a9328b8dba
Jim Berry <jimalexanderberry@gmail.com>
2021-03-22 23:30:46 +0000

    Updating types

--- pottery-app/src/types.tsx
+++ pottery-app/src/types.tsx
@@ -1,5 +1,41 @@
-export type userType = {};
+import Firebase from 'firebase';
+export type userType = {
+  firstName: string;
+  surname: string;
+  emailAddress: string;
+  artistUid: string;
+  isAdmin: boolean;
+};
 
+export type artworkType = {
+  title: string;
+  description: string;
+  artistName: string;
+  artistUid: string;
+  auctionId: string;
+  artCategory: string[];
+  artMaterials: string[];
+  views: number;
+  photosURL: string[];
+};
+
+export type auctionType = {
+  artworkId: string;
+  minimumPrice: number;
+  currentBid: number;
+  standardPrice: number;
+  auctionType: string;
+  startTimestamp: Firebase.firestore.Timestamp;
+  endTimestamp: Firebase.firestore.Timestamp;
+  sold: boolean;
+};
+
+export type aristType = {
+  firstName: string;
+  surname: string;
+  userId: string;
+  profileDescription: string;
+};
 /**
  * Types
  *
