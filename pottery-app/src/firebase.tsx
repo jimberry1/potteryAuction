b5950d9ee50ee899caf8ff80810b0ba1d142696d
Jim Berry <jimalexanderberry@gmail.com>
2021-03-22 23:31:20 +0000

    Binding in firebase

--- pottery-app/src/firebase.tsx
+++ pottery-app/src/firebase.tsx
@@ -1,1 +1,21 @@
-export {};
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyAcEI-5NRGHWOJT6zt8dnNU9SnklyaP3zc',
+  authDomain: 'craftartfair.firebaseapp.com',
+  projectId: 'craftartfair',
+  storageBucket: 'craftartfair.appspot.com',
+  messagingSenderId: '827159294520',
+  appId: '1:827159294520:web:2be324f96c5348de7e2ccd',
+  measurementId: 'G-3V85GTYCPR',
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebaseApp.storage();
+
+export { auth, firebaseApp, storage };
+export default db;
