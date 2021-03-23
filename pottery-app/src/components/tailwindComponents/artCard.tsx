09806f154f39828e5f8e6ab3121589f2261cec34
Jim Berry <jimalexanderberry@gmail.com>
2021-03-23 22:35:12 +0000

    Added base card component for art

---
+++ pottery-app/src/components/tailwindComponents/artCard.tsx
@@ -0,0 +1,51 @@
+import { artworkType } from '../../types';
+
+export interface ArtCardProps {
+  artwork?: artworkType;
+  artworkId: string;
+}
+
+const ArtCard: React.SFC<ArtCardProps> = ({ artwork, artworkId }) => {
+  return (
+    <div style={{ maxWidth: 200, height: 300 }}>
+      <a
+        href={`/artwork?artworkId=${artwork ? artworkId : ''}`}
+        aria-label="View Item"
+        className="inline-block overflow-hidden duration-300 transform bg-white rounded shadow-sm hover:-translate-y-1"
+      >
+        <div className="flex flex-col h-96 w-64">
+          <img
+            src={
+              artwork
+                ? artwork.photosURL[0]
+                : 'https://source.unsplash.com/random'
+            }
+            className="object-cover w-full h-2/3 "
+            alt=""
+          />
+          <div className=" border border-t-0 rounded-b h-1/3">
+            <div className="p-5 w-full">
+              <h6 className="mb-2 font-semibold leading-5">
+                {artwork ? artwork.title : 'Default title'}
+              </h6>
+              <p className="text-sm text-gray-900">
+                {artwork
+                  ? artwork.description
+                      .substring(
+                        0,
+                        artwork.description.length < 50
+                          ? artwork.description.length
+                          : 50
+                      )
+                      .concat('...')
+                  : 'Default item description'}
+              </p>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default ArtCard;
