992ef6a6a448048a415f6893a0a029ddc97b2511
Jim Berry <jimalexanderberry@gmail.com>
2021-03-29 23:11:06 +0100

    Adding custom art query to generic results page component

--- pottery-app/src/containers/testGenericArtContainer.tsx
+++ pottery-app/src/containers/testGenericArtContainer.tsx
@@ -3,6 +3,8 @@ import ArtCard from '../components/tailwindComponents/artCard';
 import db from '../firebase';
 import { artworkType } from '../types';
 import styled from 'styled-components';
+import { createCustomArtQuery } from '../utilities/firebaseQueries';
+import { RootStateOrAny, useSelector } from 'react-redux';
 export interface TestGenericArtContainerProps {
   artQuery?: any;
 }
@@ -12,26 +14,31 @@ const ArtContainer = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   //   margin: 120px;
+  margin-top: 15px;
 `;
 
 const TestGenericArtContainer: React.SFC<TestGenericArtContainerProps> = ({
   artQuery,
 }) => {
+  const artSearchFilters = useSelector((state: RootStateOrAny) => state.search);
   const [artResults, setArtResults]: any = useState([]);
 
+  // I need to write a query that takes the filters from the filter reducer and constructs a custom query
+  // Solution 1 would be write a query for every combination of search filter and then call that - but this has lots of manual work and isn't scalable
+
   useEffect(() => {
-    db.collection('artwork')
-      .limit(3)
-      .get()
-      .then((artQuery) => {
-        setArtResults(
-          artQuery.docs.map((artworkDoc) => ({
-            id: artworkDoc.id,
-            data: artworkDoc.data(),
-          }))
-        );
-      });
-  }, []);
+    // db.collection('artwork')
+    //   .limit(3)
+    //   .get()
+    createCustomArtQuery(artSearchFilters).then((artQuery) => {
+      setArtResults(
+        artQuery.docs.map((artworkDoc) => ({
+          id: artworkDoc.id,
+          data: artworkDoc.data(),
+        }))
+      );
+    });
+  }, [artSearchFilters]);
 
   return (
     <ArtContainer>
