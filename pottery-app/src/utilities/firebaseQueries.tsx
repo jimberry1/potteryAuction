cefbf8052b376d9308739b153997564fabe0a7ff
Jim Berry <jimalexanderberry@gmail.com>
2021-03-29 23:09:23 +0100

    Adding firebase query for custom art searching

--- pottery-app/src/utilities/firebaseQueries.tsx
+++ pottery-app/src/utilities/firebaseQueries.tsx
@@ -1,10 +1,11 @@
 import db from '../firebase';
-import { artworkType } from '../types';
+import { artworkType, searchStateType } from '../types';
 import {
   ARTWORK_TABLE,
   FIREBASE_GENERAL_INFO_TABLE,
 } from './firebaseQueryConfig';
 import Firebase from 'firebase';
+import { ART_MATERIALS } from '../configuration/staticVariableNames/databaseTableAndFieldNames';
 
 export const fetchGeneralInfoTableByDocumentId = async (documentId: string) => {
   return await db.collection(FIREBASE_GENERAL_INFO_TABLE).doc(documentId).get();
@@ -25,4 +26,34 @@ export const addViewToArtworkWhenLoaded = (artworkId: string) => {
     .doc(artworkId)
     .update({ views: Firebase.firestore.FieldValue.increment(1) })
     .catch((err) => console.log(err));
+};
+
+export const createCustomArtQuery = async (searchFilters: searchStateType) => {
+  let dbQuery = db.collection(ARTWORK_TABLE);
+  if (searchFilters.materialSelection) {
+    const materialArrayForWorkaround = [searchFilters.materialSelection];
+    console.log(materialArrayForWorkaround);
+    dbQuery = addArtMaterialFilter(dbQuery, searchFilters.materialSelection);
+  }
+  if (searchFilters.sizeSelection) {
+    dbQuery = addSizeFilter(dbQuery, searchFilters.sizeSelection);
+  }
+  if (searchFilters.artType) {
+    dbQuery = addArtTypeFilter(dbQuery, searchFilters.artType);
+  }
+  // ... add more queries here
+
+  return await dbQuery.limit(20).get();
+};
+
+const addArtMaterialFilter = (query: any, artMaterial: string) => {
+  return query.where(ART_MATERIALS, 'array-contains', artMaterial);
+};
+
+const addSizeFilter = (query: any, size: string) => {
+  return query.where('size', '==', size);
+};
+
+const addArtTypeFilter = (query: any, artType: string) => {
+  return query.where('artCategory', '==', artType);
 };
