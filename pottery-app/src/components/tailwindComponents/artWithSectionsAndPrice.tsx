2dc2a7cd29e4acbab4eaa6375779f18cccfa22fd
Jim Berry <jimalexanderberry@gmail.com>
2021-03-31 23:39:44 +0100

    Improving art page to have navigatable sections

--- pottery-app/src/components/tailwindComponents/artWithSectionsAndPrice.tsx
+++ pottery-app/src/components/tailwindComponents/artWithSectionsAndPrice.tsx
@@ -1,13 +1,50 @@
+import { useState } from 'react';
 import { artworkType } from '../../types';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { artPageFadeInVariants } from '../../styles/animations';
+import ArtCardSubHeader from './artCardSectionSubComponents/artCardSubHeader';
+import {
+  ARTIST,
+  CHARITY,
+  DESCRIPTION,
+} from '../../configuration/staticVariableNames/artPageTabAndFieldNames';
+import PointyArrowWithText from '../pointArrowWithText';
 export interface ArtWithSectionsAndPriceProps {
   artwork: artworkType;
 }
 
-const ArtWithSectionsAndPrice: React.SFC<ArtWithSectionsAndPriceProps> = ({
+export interface ArtComponentSubSection {
+  artwork: artworkType;
+}
+
+type category = {
+  label: string;
+  id: string;
+};
+
+const subComponentVairants = {
+  hidden: {
+    y: 30,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      easing: 'easeInOut',
+    },
+  },
+};
+
+const ArtWithSectionsAndPrice: React.FunctionComponent<ArtWithSectionsAndPriceProps> = ({
   artwork,
 }) => {
+  const categories: category[] = [
+    { id: '98234894389j', label: 'Description' },
+    { id: 'fkj32ofi23i3f', label: 'Artist' },
+    { id: 'fklfipf3i390', label: 'Charity' },
+  ];
+  const [selectedCategory, setSelectedCategory] = useState('Description');
   return (
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-2 md:py-24 mx-auto">
@@ -23,42 +60,42 @@ const ArtWithSectionsAndPrice: React.SFC<ArtWithSectionsAndPriceProps> = ({
             src={artwork.photosURL[0]}
           />
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mb-6 lg:mb-0">
-            <h2 className="text-sm title-font text-gray-500 tracking-widest">
-              ARTIST NAME
-            </h2>
             <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
               {artwork.title}
             </h1>
             <div className="flex mb-4">
-              <a className="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-lg px-1">
-                Description
-              </a>
-              <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">
-                Artist
-              </a>
-              <a className="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">
-                Charity
-              </a>
+              {categories.map((category) => {
+                return (
+                  <ArtCardSubHeader
+                    subtitle={category.label}
+                    key={category.id}
+                    selected={category.label === selectedCategory}
+                    clicked={() => setSelectedCategory(category.label)}
+                  />
+                );
+              })}
             </div>
-            <p className="leading-relaxed mb-4">{artwork.description}</p>
-            <div className="flex border-t border-gray-400 py-2">
-              <span className="text-gray-500">Height</span>
-              <span className="ml-auto text-gray-900">120 cm</span>
-            </div>
-            <div className="flex border-t border-gray-400 py-2">
-              <span className="text-gray-500">Width</span>
-              <span className="ml-auto text-gray-900">30 cm</span>
-            </div>
-            <div className="flex border-t border-b mb-6 border-gray-400 py-2">
-              <span className="text-gray-500">Views</span>
-              <span className="ml-auto text-gray-900">{artwork.views}</span>
-            </div>
-            <div className="flex">
+
+            <AnimatePresence exitBeforeEnter>
+              {selectedCategory === DESCRIPTION && (
+                <DescriptionSubSection artwork={artwork} key="234892jwdjw209" />
+              )}
+              {selectedCategory === ARTIST && (
+                <ArtistSubSection artwork={artwork} key="234892jwdj2242w209" />
+              )}
+              {selectedCategory === CHARITY && (
+                <CharitySubSection
+                  artwork={artwork}
+                  key="234892jw234221djw209"
+                />
+              )}
+            </AnimatePresence>
+            <div className="flex mt-5">
               <span className="title-font font-medium text-2xl text-gray-900">
                 $58.00
               </span>
               <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                Button
+                Buy
               </button>
               <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                 <svg
@@ -81,3 +118,73 @@ const ArtWithSectionsAndPrice: React.SFC<ArtWithSectionsAndPriceProps> = ({
 };
 
 export default ArtWithSectionsAndPrice;
+
+const DescriptionSubSection: React.FunctionComponent<ArtWithSectionsAndPriceProps> = ({
+  artwork,
+}) => {
+  return (
+    <motion.div
+      variants={subComponentVairants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      style={{ minHeight: 200 }}
+    >
+      <p className="leading-relaxed mb-4">{artwork.description}</p>
+      <div className="flex border-t border-gray-400 py-2">
+        <span className="text-gray-500">Height</span>
+        <span className="ml-auto text-gray-900">120 cm</span>
+      </div>
+      <div className="flex border-t border-gray-400 py-2">
+        <span className="text-gray-500">Width</span>
+        <span className="ml-auto text-gray-900">30 cm</span>
+      </div>
+      <div className="flex border-t border-b mb-6 border-gray-400 py-2">
+        <span className="text-gray-500">Views</span>
+        <span className="ml-auto text-gray-900">{artwork.views}</span>
+      </div>
+    </motion.div>
+  );
+};
+
+const CharitySubSection: React.FunctionComponent<ArtWithSectionsAndPriceProps> = ({
+  artwork,
+}) => {
+  return (
+    <motion.div
+      variants={subComponentVairants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      style={{ minHeight: 200 }}
+    >
+      <h2 className="text-xl font-bold title-font text-gray-500 tracking-widest pb-1">
+        {artwork.charityName}
+      </h2>
+      <p className="leading-relaxed mb-4">{artwork.charityDescription}</p>
+      <PointyArrowWithText text={artwork.charityURL} clicked={() => {}} />
+    </motion.div>
+  );
+};
+
+const ArtistSubSection: React.FunctionComponent<ArtWithSectionsAndPriceProps> = ({
+  artwork,
+}) => {
+  return (
+    <motion.div
+      variants={subComponentVairants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      style={{ minHeight: 200 }}
+    >
+      <h2 className="text-xl font-bold title-font text-gray-500 tracking-widest pb-1">
+        {artwork.artistName}
+      </h2>
+      <p className="leading-relaxed mb-4">
+        This is the section where information about the artist will be displayed
+      </p>
+      <PointyArrowWithText text="See more" clicked={() => {}} />
+    </motion.div>
+  );
+};
