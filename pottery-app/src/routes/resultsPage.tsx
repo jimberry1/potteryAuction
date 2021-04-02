9b36902cd9f63df03857b4e795664d101ceaa18e
Jim Berry <jimalexanderberry@gmail.com>
2021-04-02 13:07:46 +0100

    Adding url interpretation and smart results sets to results page

--- pottery-app/src/routes/resultsPage.tsx
+++ pottery-app/src/routes/resultsPage.tsx
@@ -1,13 +1,26 @@
-import SearchFilterBar from '../components/searchFilterBar';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
 import ArtSearchResultContainer from '../containers/artSearchResult/artSearchResultContainer';
-import TestGenericArtContainer from '../containers/artSearchResult/testGenericArtContainer';
-
+import { useLocation } from 'react-router';
+import ArtistSearchResultContainer from '../containers/artistSearchResult/artistSearchResultContainer';
 export interface ResultsPageProps {}
 
 const ResultsPage: React.SFC<ResultsPageProps> = () => {
+  const location = useLocation();
+  const [category, setCategory] = useState('');
+
+  useEffect(() => {
+    const queryString = require('query-string');
+    const parsed = queryString.parse(location.search);
+    console.log(parsed);
+    if (parsed.category) {
+      setCategory(parsed.category);
+    }
+  }, [location]);
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <ArtSearchResultContainer />
+      {category === 'artwork' && <ArtSearchResultContainer />}
+      {category === 'artist' && <ArtistSearchResultContainer />}
     </div>
   );
 };
