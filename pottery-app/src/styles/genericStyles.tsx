cac1abe8bcbcb85b3e2d3279bc71310a3f5e7ece
Jim Berry <jimalexanderberry@gmail.com>
2021-04-02 12:13:55 +0100

    Adding generic styles

---
+++ pottery-app/src/styles/genericStyles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ArtContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  //   margin: 120px;
+  margin-top: 15px;
+`;
