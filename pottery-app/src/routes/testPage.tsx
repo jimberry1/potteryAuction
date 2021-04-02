2ce2db3a55bbc9e55ef1f6125e12fdfe57a8417d
Jim Berry <jimalexanderberry@gmail.com>
2021-04-02 12:16:15 +0100

    Adding fake data to test page

--- pottery-app/src/routes/testPage.tsx
+++ pottery-app/src/routes/testPage.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { artworkType } from '../types';
-import { addArtworkToArtworkDatabaseTable } from '../utilities/firebaseQueries';
+import { artistType, artworkType } from '../types';
+import Modal from '../UI/modal';
+import {
+  addArtistToFirebase,
+  addArtworkToArtworkDatabaseTable,
+} from '../utilities/firebaseQueries';
 
 export interface TestPageProps {}
 
@@ -10,7 +14,7 @@ const TestPage: React.FunctionComponent<TestPageProps> = () => {
       title: 'blah',
       description: 'desc',
       artistName: 'jim',
-      artistUid: 'testUID',
+      artistUid: 'AqbGwOUneeNiIZzwvLNZ',
       auctionId: 'auctionUID',
       artCategory: 'Pottery',
       artMaterials: ['Clay', 'butter'],
@@ -20,8 +24,27 @@ const TestPage: React.FunctionComponent<TestPageProps> = () => {
       charityName: 'British Heart Foundation',
       charityDescription: 'This charity means a lot to me',
       charityURL: 'https://www.bhf.org.uk/',
+      sold: false,
     };
     addArtworkToArtworkDatabaseTable(dummyArtwork)
+      .catch((err) => console.log(err))
+      .then(() => {
+        console.log('success');
+      });
+  };
+
+  const sendDummyArtistDataToFirebase = () => {
+    const fakeArtist: artistType = {
+      firstName: 'Jim',
+      surname: 'Berry',
+      userId: '',
+      profileDescription: 'This is my art page where I make and sell art',
+      photoURL:
+        'https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+      location: 'London',
+    };
+
+    addArtistToFirebase(fakeArtist)
       .catch((err) => console.log(err))
       .then(() => {
         console.log('success');
@@ -37,6 +60,14 @@ const TestPage: React.FunctionComponent<TestPageProps> = () => {
         >
           Add dummy artwork
         </button>
+        <button
+          type="button"
+          className="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-blue-500 hover:bg-blue-600 hover:shadow-lg"
+          onClick={sendDummyArtistDataToFirebase}
+        >
+          Add dummy artist
+        </button>
+        <Modal />
       </div>
     </div>
   );
