f0aa2f1d8d92533884fa2f13e85776ce57baefcc
Jim Berry <jimalexanderberry@gmail.com>
2021-04-02 12:14:38 +0100

    Adding utility function for generating art statistics

--- pottery-app/src/utilities/utilityFunctions.tsx
+++ pottery-app/src/utilities/utilityFunctions.tsx
@@ -1,3 +1,5 @@
+import { ArtistProfileArtworkStatistics, artworkType } from '../types';
+
 export const formatItemDescriptionForCard: (description: string) => string = (
   description
 ) => {
@@ -12,4 +14,26 @@ export const formatItemTitleForCard: (title: string) => string = (title) => {
   if (title.length < 40) {
     return title;
   } else return title.substring(0, 40).concat('...');
+};
+
+export const generateArtistStatsFromArtworkArray: (
+  artworks: { id: string; data: artworkType }[]
+) => ArtistProfileArtworkStatistics = (artworks) => {
+  if (!artworks[0]) {
+    return {
+      totalViews: 0,
+      totalEarnedFromSales: 0,
+      totalSales: 0,
+      artworkCount: 0,
+    };
+  }
+  const reducedViews = artworks.map((artwork) => artwork.data.views);
+  return {
+    totalViews: artworks
+      .map((artwork) => artwork.data.views)
+      .reduce((total = 0, views) => total + views),
+    totalEarnedFromSales: 0,
+    totalSales: artworks.filter((artwork) => artwork.data.sold).length,
+    artworkCount: artworks.length,
+  };
 };
