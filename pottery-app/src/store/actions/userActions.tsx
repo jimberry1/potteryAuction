466c0e0c260ddd204b7376a407fbf674d02fe796
Jim Berry <jimalexanderberry@gmail.com>
2021-04-05 23:55:18 +0100

    Adding user actions for handling auth with redux

--- pottery-app/src/store/actions/userActions.tsx
+++ pottery-app/src/store/actions/userActions.tsx
@@ -1,6 +1,17 @@
 import { userType } from '../../types';
 import * as actions from './actionTypes';
+import Firebase from 'firebase';
 
 export const setUser = (userId: string, user: userType) => {
   return { type: actions.SET_USER, userId: userId, user: user };
+};
+
+export const setUserFromFirebaseUserSnapshot = (
+  userSnapshot: Firebase.firestore.DocumentSnapshot
+) => {
+  return {
+    type: actions.SET_USER,
+    userId: userSnapshot.id,
+    user: userSnapshot.data(),
+  };
 };
