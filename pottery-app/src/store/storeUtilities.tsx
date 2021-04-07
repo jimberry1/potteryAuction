ce940c1daa62194db35fb0b5acde8810a946cc57
Jim Berry <jimalexanderberry@gmail.com>
2021-04-07 21:29:01 +0100

    Adding two new selector queries

--- pottery-app/src/store/storeUtilities.tsx
+++ pottery-app/src/store/storeUtilities.tsx
@@ -10,6 +10,11 @@ export const updateObject = (oldObject: any, updatedProperties: any) => {
 export const searchStateSelector = (state: RootStateOrAny) => state.search;
 
 export const adminStateSelector = (state: RootStateOrAny) => state.admin;
+export const adminShowAuthenticationContainerSelector = (
+  state: RootStateOrAny
+) => state.admin.showAuth;
 
 export const userStateSelector = (state: RootStateOrAny) => state.user;
 export const userIdStateSelector = (state: RootStateOrAny) => state.user.userId;
+export const userArtistIdStateSelector = (state: RootStateOrAny) =>
+  state.user.user.artistUid;
