7b2c5d9bf6a5d7a718c399bf0c1f05f2f67ae83f
Jim Berry <jimalexanderberry@gmail.com>
2021-04-08 22:55:19 +0100

    Adding new config

--- pottery-app/src/configuration/staticVariableNames/databaseTableAndFieldNames.tsx
+++ pottery-app/src/configuration/staticVariableNames/databaseTableAndFieldNames.tsx
@@ -2,3 +2,6 @@ export const ART_MATERIALS = 'artMaterials';
 export const ART_CATEGORY = 'artCategory';
 
 export const LOCAL_STORAGE_ID_KEY = 'caf_uid';
+
+export const BID_AUCTION_TYPE = 'BID';
+export const FIXED_PRICE_AUCTION_TYPE = 'FIXED_PRICE';
