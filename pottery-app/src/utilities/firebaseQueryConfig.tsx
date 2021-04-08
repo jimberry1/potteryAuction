6b916632586b62cc44170cc8daebc307e42513b9
Jim Berry <jimalexanderberry@gmail.com>
2021-04-08 22:53:23 +0100

    Adding completed auction config to firebase config

--- pottery-app/src/utilities/firebaseQueryConfig.tsx
+++ pottery-app/src/utilities/firebaseQueryConfig.tsx
@@ -3,5 +3,7 @@ export const FIREBASE_GENERAL_INFO_TABLE = 'general';
 export const ARTWORK_TABLE = 'artwork';
 export const ARTIST_TABLE = 'artist';
 export const USER_TABLE = 'users';
+export const AUCTION_TABLE = 'auctions';
+export const COMPLETED_AUCTION_TABLE = 'completedAuctions';
 // Table documents
 export const LANDING_PAGE_INFO_DOC = 'landingPageInfo';
