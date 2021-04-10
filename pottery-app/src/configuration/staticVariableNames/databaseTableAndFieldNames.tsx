5260a23db1142a36d4c5579542e0a6cd92efd26d
Jim Berry <jimalexanderberry@gmail.com>
2021-04-10 10:39:27 +0100

    Updating static config

--- pottery-app/src/configuration/staticVariableNames/databaseTableAndFieldNames.tsx
+++ pottery-app/src/configuration/staticVariableNames/databaseTableAndFieldNames.tsx
@@ -4,4 +4,4 @@ export const ART_CATEGORY = 'artCategory';
 export const LOCAL_STORAGE_ID_KEY = 'caf_uid';
 
 export const BID_AUCTION_TYPE = 'BID';
-export const FIXED_PRICE_AUCTION_TYPE = 'FIXED_PRICE';
+export const BUYOUT_AUCTION_TYPE = 'BUYOUT';
